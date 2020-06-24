c570e4bf368235b18b9ac6ab9f554a44c9398092
Jhoseph Araujo <j.garibald@gmail.com>
2020-06-24 18:25:38 -0300

    backend utilizando dados em memoria

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express, { json } from 'express';
 import routes from './routes';
 
 const app = express();
-
-app.get('/', (request, response) => response.json({ message: 'Server ok' }));
+app.use(json());
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('>>>Server started on port 3333!');
