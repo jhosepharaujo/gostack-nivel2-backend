754ddd30790a2cd9d1e715a6395472095be21f08
Jhoseph Araujo <j.garibald@gmail.com>
2020-06-24 13:05:59 -0300

    Alterado mensagem de inicializaçao do servidor

--- src/server.ts
+++ src/server.ts
@@ -6,5 +6,5 @@ const app = express();
 app.get('/', (request, response) => response.json({ message: 'Server ok' }));
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333!');
+    console.log('>>>Server started on port 3333!');
 });
