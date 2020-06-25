a9b3ea09f51756ee2022c86450c422b5ab314050
Jhoseph Araujo <j.garibald@gmail.com>
2020-06-25 00:30:20 -0300

    Adicionada trativa global para erros da aplicacao

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,25 @@
 import 'reflect-metadata';
-import express, { json } from 'express';
+import express, { json, Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import routes from './routes';
 import './database';
+import AppError from './errors/AppError';
 
 const app = express();
 app.use(json());
 app.use(routes);
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+    if (err instanceof AppError) {
+        response
+            .status(err.statusCode)
+            .json({ status: 'error', message: err.message });
+    }
+
+    return response.status(500).json({
+        status: 'error',
+        message: 'Internal server error',
+    });
+});
 
 app.listen(3333, () => {
     console.log('>>>Server started on port 3333!');
