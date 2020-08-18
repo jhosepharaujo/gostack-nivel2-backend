d159ccca7532a2efd871fc50fcff1c7f1e512fec
Jhoseph Araujo <j.garibald@gmail.com>
2020-08-18 09:48:55 -0300

    Adicionada funcionalidade de listar agendamentos do provedor de servico

---
+++ src/modules/appointments/services/ListProviderAppointmentsService.spec.ts
@@ -0,0 +1,37 @@
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderAppointmentsService from './ListProviderAppointmentsService';
+
+let listProviderAppointmentsService: ListProviderAppointmentsService;
+let fakeAppontmentsRepository: FakeAppointmentsRepository;
+
+describe('ListProviderAppointmentsService', () => {
+    beforeEach(() => {
+        fakeAppontmentsRepository = new FakeAppointmentsRepository();
+        listProviderAppointmentsService = new ListProviderAppointmentsService(
+            fakeAppontmentsRepository,
+        );
+    });
+
+    it('should be able to list the appointments on a specific day', async () => {
+        const appointment1 = await fakeAppontmentsRepository.create({
+            provider_id: 'provider',
+            user_id: 'user',
+            date: new Date(2020, 7, 18, 14, 0, 0),
+        });
+
+        const appointment2 = await fakeAppontmentsRepository.create({
+            provider_id: 'provider',
+            user_id: 'user',
+            date: new Date(2020, 7, 18, 15, 0, 0),
+        });
+
+        const appointments = await listProviderAppointmentsService.execute({
+            provider_id: 'provider',
+            year: 2020,
+            month: 8,
+            day: 18,
+        });
+
+        expect(appointments).toEqual([appointment1, appointment2]);
+    });
+});
