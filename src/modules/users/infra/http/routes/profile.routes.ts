7c756f24075f6a457a474b9ccbc8fb9fd8cf8a43
Jhoseph Araujo <j.garibald@gmail.com>
2020-08-18 12:09:12 -0300

    Adicionada validacao de dados para as rotas

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import ProfileController from '../controllers/ProfileController';
 
 import ensureAuthenticated from '../middleware/ensureAuthenticated';
@@ -9,6 +10,18 @@ profileRouter.use(ensureAuthenticated);
 const profilecontroller = new ProfileController();
 
 profileRouter.get('/', profilecontroller.show);
-profileRouter.put('/', profilecontroller.update);
+profileRouter.put(
+    '/',
+    celebrate({
+        [Segments.BODY]: {
+            name: Joi.string().required(),
+            email: Joi.string().required().email(),
+            old_password: Joi.string(),
+            password: Joi.string(),
+            password_confirmation: Joi.string().valid(Joi.ref('password')),
+        },
+    }),
+    profilecontroller.update,
+);
 
 export default profileRouter;
